4d25cfa278e89e348f39532565bcad3b564598af
bmalang80-design <bmalang@outlook.com>
2020-02-04 20:58:16 -0500

    all the button are working now

--- app/src/main/java/com/example/ping2pong/GameBoard.java
+++ app/src/main/java/com/example/ping2pong/GameBoard.java
@@ -1,14 +1,55 @@
 package com.example.ping2pong;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GameBoard extends AppCompatActivity {
+import java.util.Locale;
 
+public class GameBoard extends AppCompatActivity {
+    boolean isRunning = false;
+    long countDown ;
+    TextView displayFinal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_board);
+        Bundle extra = getIntent().getExtras();
+        int countScore = extra.getInt("countScore", 10);
+        int countTime = extra.getInt("countTime", 5);
+        init();
+        timeStart(countTime * 10000);
+
+
+
+    }
+    private void init(){
+        displayFinal = findViewById(R.id.display);
+
+    }
+    private void timeStart(long count){
+     new CountDownTimer( count,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                countDown = millisUntilFinished;
+                updateTime();
+            }
+
+            @Override
+            public void onFinish() {
+                displayFinal.setText("Game over");
+            }
+        }.start();
+        isRunning = true;
+    }
+    private void updateTime() {
+        long min = ((countDown / 1000) / 60);
+        long sec = ((countDown / 1000) % 60);
+        String fortTime = String.format(Locale.getDefault(), "%02d:%02d", min, sec);
+       displayFinal.setText(fortTime);
     }
 }
--- app/src/main/java/com/example/ping2pong/MainActivity.java
+++ app/src/main/java/com/example/ping2pong/MainActivity.java
@@ -1,14 +1,90 @@
 package com.example.ping2pong;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    int countScore = 0;
+    int countTime = 0;
+    Button startMinus;
+    Button startAdd;
+    Button timeAdd;
+    Button timeMinus;
+    Button exits;
+    Button start;
+    TextView displayScores;
+    TextView displayTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        buttonsPress();
+    }
+    private void init(){
+        start = findViewById(R.id.start);
+        startMinus = findViewById(R.id.startMinus);
+        startAdd = findViewById(R.id.startAdd);
+        timeAdd = findViewById(R.id.timeAdd);
+        timeMinus = findViewById(R.id.timeMinus);
+        exits = findViewById(R.id.exits);
+        displayScores = findViewById(R.id.displayScores);
+        displayTime = findViewById(R.id.displayTime);
+    }
+    private void buttonsPress(){
+        startMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (countScore > 1){
+                    countScore--;
+                }
+                displayScores.setText(countScore + "");
+            }
+        });
+        startAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (countScore < 10){
+                    countScore++;
+                }
+                displayScores.setText(countScore +"");
+            }
+        });
+        timeMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (countTime > 1){
+                    countTime--;
+                }
+                displayTime.setText(countTime + "");
+            }
+        });
+        timeAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (countTime < 10){
+                    countTime++;
+                }
+                displayTime.setText(countTime +"");
+            }
+        });
+    }
+
+    public void start(View view) {
+        Intent intent = new Intent(this, GameBoard.class);
+        intent.putExtra("countScore", countScore);
+        intent.putExtra("countTime", countTime);
+        startActivity(intent);
+    }
+
+    public void exits(View view) {
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(0);
     }
 }
